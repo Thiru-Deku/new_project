ec738f1459b00b7477f88bbb7edd4950325e0c9f
thiru <explorerenigma@gmail.com>
2024-06-26 22:30:28 +0530

    Architect development

---
+++ src/new_package/newProgram.java
@@ -0,0 +1,11 @@
+package new_package;
+
+public class newProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a program");
+
+	}
+
+}
